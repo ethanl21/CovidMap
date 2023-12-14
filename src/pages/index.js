4c0ecfcf3e17693d0e2369ff4a68f996478a8453
Ethan Lew <15167713+ethanl21@users.noreply.github.com>
2023-12-14 12:42:07 -0800

    Add formatted tooltip
    (this is where the video tutorial ends)

--- src/pages/index.js
+++ src/pages/index.js
@@ -5,14 +5,13 @@ import L from 'leaflet';
 import axios from 'axios';
 
 //import { promiseToFlyTo, getCurrentLocation } from 'lib/map';
-
 import Layout from 'components/Layout';
 import Container from 'components/Container';
 import Map from 'components/Map';
 
 const LOCATION = {
-  lat: 38.9072,
-  lng: -77.0369,
+  lat: 0,
+  lng: 0,
 };
 const CENTER = [LOCATION.lat, LOCATION.lng];
 const DEFAULT_ZOOM = 2;
@@ -63,8 +62,51 @@ const IndexPage = () => {
       }),
     };
 
+    function countryPointToLayer( feature = {}, latlng ) {
+      const { properties = {} } = feature;
+      let updateFormatted;
+      let caseString;
+
+      // destructure data
+      const { country, updated, cases, deaths, recovered } = properties;
+
+      caseString = `${cases}`;
+
+      if ( cases > 1000 ) {
+        caseString = `${caseString.slice( 0, -3 )}k+`;
+      }
+
+      if ( updated ) {
+        updateFormatted = new Date( updated ).toLocaleString();
+      }
+
+      const html = `
+<span class="icon-marker">
+    <span class="icon-marker-tooltip">
+        <h2>${country}</h2>
+        <ul>
+            <li><strong>Confirmed: </strong>${cases}</li>
+            <li><strong>Deaths: </strong>${deaths}</li>
+            <li><strong>Recovered: </strong>${recovered}</li>
+            <li><strong>Last Update: </strong>${updateFormatted}</li>
+        </ul>
+    </span>
+    ${caseString}
+</span>
+`;
+      return L.marker( latlng, {
+        icon: L.divIcon({
+          className: 'icon',
+          html,
+        }),
+        riseOnHover: true,
+      });
+    }
+
     // Insert GeoJSON into map
-    const geoJSONLayers = new L.GeoJSON( geoJSON );
+    const geoJSONLayers = new L.GeoJSON( geoJSON, {
+      pointToLayer: countryPointToLayer,
+    });
     geoJSONLayers.addTo( map );
   }
 
