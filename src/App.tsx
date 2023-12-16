693a8b486dc2a1dafb55574ff1da4e4c60344811
Ethan Lew <15167713+ethanl21@users.noreply.github.com>
2023-12-15 20:44:07 -0800

    Share selected state with graphs

--- src/App.tsx
+++ src/App.tsx
@@ -1,5 +1,5 @@
 import { useState } from "react";
-import { HTMLSelect, FormGroup, Card } from "@blueprintjs/core";
+import { HTMLSelect, FormGroup } from "@blueprintjs/core";
 import { USAMap } from "./components/Map";
 import { Dashboard } from "./components/CovidDashboard";
 import { Graphs } from "./components/Graphs";
@@ -12,30 +12,25 @@ function App() {
 
   return (
     <>
-      <div
-        style={{
-          height: "80svh",
-          margin: "2rem",
-          borderRadius: "1rem",
-          borderBottomRightRadius: "unset",
-          overflow: "hidden",
-        }}
-      > 
-        <USAMap mode={mapState} />
-        
-
-      </div>
-      
-      <HTMLSelect
-        defaultValue={mapState}
-        onChange={(e) => setMapState(e.target.value)}
-      >
-        <option value={"county"}>Counties</option>
-        <option value={"states"}>States</option>
-      </HTMLSelect>
-      <Dashboard/>
       <div style={{ padding: "1rem" }}>
-        <h1>Map</h1>
+        <div
+          style={{
+            display: "flex",
+            justifyContent: "space-between",
+            alignItems: "start",
+          }}
+        >
+          <h1>Map</h1>
+          <FormGroup label="Mode">
+            <HTMLSelect
+              defaultValue={mapState}
+              onChange={(e) => setMapState(e.target.value)}
+            >
+              <option value={"county"}>Counties</option>
+              <option value={"states"}>States</option>
+            </HTMLSelect>
+          </FormGroup>
+        </div>
         <div
           style={{
             height: "80svh",
@@ -49,41 +44,58 @@ function App() {
         </div>
       </div>
 
-      <div style={{ padding: "1rem", width: "20rem" }}>
-        <Card interactive>
-          <h2>Options</h2>
-          <FormGroup label="Mode">
-            <HTMLSelect
-              defaultValue={mapState}
-              onChange={(e) => setMapState(e.target.value)}
-            >
-              <option value={"county"}>Counties</option>
-              <option value={"states"}>States</option>
-            </HTMLSelect>
-          </FormGroup>
-        </Card>
+      <div
+        style={{
+          padding: "1rem",
+          display: "flex",
+          justifyContent: "space-between",
+          alignItems: "start",
+        }}
+      >
+        <h1>Tables</h1>
+        <FormGroup label="State">
+          <HTMLSelect
+            value={selectedState}
+            onChange={(e) => setSelectedState(e.target.value)}
+          >
+            {Object.entries(statesAbbr).map((state, index) => {
+              return (
+                <option value={state[1]} key={index}>
+                  {state[0]}
+                </option>
+              );
+            })}
+          </HTMLSelect>
+        </FormGroup>
+      </div>
+      <div style={{ padding: "1rem" }}>
+        <Dashboard stateCode={selectedState} />
       </div>
 
       <div style={{ padding: "1rem" }}>
-        <h1>Charts</h1>
-        <div style={{ padding: "1rem", width: "20rem" }}>
-          <Card interactive>
-            <h2>Options</h2>
-            <FormGroup label="State">
-              <HTMLSelect
-                value={selectedState}
-                onChange={(e) => setSelectedState(e.target.value)}
-              >
-                {Object.entries(statesAbbr).map((state, index) => {
-                  return (
-                    <option value={state[1]} key={index}>
-                      {state[0]}
-                    </option>
-                  );
-                })}
-              </HTMLSelect>
-            </FormGroup>
-          </Card>
+        <div
+          style={{
+            padding: "1rem",
+            display: "flex",
+            justifyContent: "space-between",
+            alignItems: "start",
+          }}
+        >
+          <h1>Charts</h1>
+          <FormGroup label="State">
+            <HTMLSelect
+              value={selectedState}
+              onChange={(e) => setSelectedState(e.target.value)}
+            >
+              {Object.entries(statesAbbr).map((state, index) => {
+                return (
+                  <option value={state[1]} key={index}>
+                    {state[0]}
+                  </option>
+                );
+              })}
+            </HTMLSelect>
+          </FormGroup>
         </div>
         <Graphs stateCode={selectedState} />
       </div>
