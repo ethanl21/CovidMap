d797c5a36948876d04217334f651f7027dbfa2ea
Ethan Lew <15167713+ethanl21@users.noreply.github.com>
2023-12-15 18:00:35 -0800

    Split out components from Map

---
+++ src/lib/API.ts
@@ -0,0 +1,18 @@
+export interface StateData {
+  cases: number;
+  date: string;
+  deaths: number;
+  fips: string;
+  state: string;
+  updated: number;
+}
+
+export interface CountyData {
+  cases: number;
+  county: string;
+  date: string;
+  deaths: number;
+  fips: string;
+  state: string;
+  updated: number;
+}
