45e3c6764e9bd00ca5060a0b84c31ce2a6a153c2
Ethan Lew <15167713+ethanl21@users.noreply.github.com>
2023-12-15 21:26:34 -0800

    Add CSS styles

--- src/App.tsx
+++ src/App.tsx
@@ -11,97 +11,172 @@ function App() {
 
   return (
     <>
-      <div style={{ padding: "1rem" }}>
-        <div
-          style={{
-            display: "flex",
-            justifyContent: "space-between",
-            alignItems: "start",
-          }}
-        >
-          <h1>Map</h1>
-          <FormGroup label="Mode">
-            <HTMLSelect
-              defaultValue={mapState}
-              onChange={(e) => setMapState(e.target.value)}
-            >
-              <option value={"county"}>Counties</option>
-              <option value={"states"}>States</option>
-            </HTMLSelect>
-          </FormGroup>
-        </div>
-        <div
-          style={{
-            height: "80svh",
-            margin: "2rem",
-            borderRadius: "1rem",
-            borderBottomRightRadius: "unset",
-            overflow: "hidden",
-          }}
-        >
-          <USAMap mode={mapState} />
-        </div>
-      </div>
-
-      <div
-        style={{
-          padding: "1rem",
-          display: "flex",
-          justifyContent: "space-between",
-          alignItems: "start",
-        }}
-      >
-        <h1>
-          Tables <a href="https://covidactnow.org">[source]</a>
-        </h1>
-        <FormGroup label="State">
-          <HTMLSelect
-            value={selectedState}
-            onChange={(e) => setSelectedState(e.target.value)}
+      <header>
+        <div style={{ backgroundColor: "lightgray" }}>
+          <div
+            style={{
+              width: "100%",
+              display: "flex",
+              flexDirection: "column",
+              justifyContent: "center",
+              alignItems: "center",
+            }}
           >
-            {Object.entries(statesAbbr).map((state, index) => {
-              return (
-                <option value={state[1]} key={index}>
-                  {state[0]}
-                </option>
-              );
-            })}
-          </HTMLSelect>
-        </FormGroup>
-      </div>
-      <div style={{ padding: "1rem" }}>
-        <Dashboard stateCode={selectedState} />
-      </div>
+            <h1 style={{ fontSize: "3rem", marginBottom: "0" }}>
+              🦠 CovidMap 💉
+            </h1>
+            <h2>
+              <em>
+                ✨ by Allan Cortes, Raquel Cruz, Jose Gonzalez, and Ethan Lew ✨
+              </em>
+            </h2>
+          </div>
+        </div>
+      </header>
 
-      <div style={{ padding: "1rem" }}>
+      <main>
         <div
           style={{
             padding: "1rem",
-            display: "flex",
-            justifyContent: "space-between",
-            alignItems: "start",
+            backgroundColor: "white",
+            margin: "2rem",
+            borderRadius: "1rem",
           }}
         >
-          <h1>
-            Charts <a href="https://covidactnow.org">[source]</a>
-          </h1>
-          <FormGroup label="State">
-            <HTMLSelect
-              value={selectedState}
-              onChange={(e) => setSelectedState(e.target.value)}
-            >
-              {Object.entries(statesAbbr).map((state, index) => {
-                return (
-                  <option value={state[1]} key={index}>
-                    {state[0]}
-                  </option>
-                );
-              })}
-            </HTMLSelect>
-          </FormGroup>
+          <div
+            style={{
+              display: "flex",
+              justifyContent: "space-between",
+              alignItems: "start",
+            }}
+          >
+            <h1>Map</h1>
+            <FormGroup label="Mode">
+              <HTMLSelect
+                defaultValue={mapState}
+                onChange={(e) => setMapState(e.target.value)}
+              >
+                <option value={"county"}>Counties</option>
+                <option value={"states"}>States</option>
+              </HTMLSelect>
+            </FormGroup>
+          </div>
+          <div
+            style={{
+              height: "80svh",
+              margin: "2rem",
+              borderRadius: "1rem",
+              borderBottomRightRadius: "unset",
+              overflow: "hidden",
+            }}
+          >
+            <USAMap mode={mapState} />
+          </div>
         </div>
-        <Graphs stateCode={selectedState} />
-      </div>
+
+        <div
+          style={{
+            backgroundColor: "white",
+            margin: "2rem",
+            borderRadius: "1rem",
+          }}
+        >
+          <div
+            style={{
+              padding: "1rem",
+              display: "flex",
+              justifyContent: "space-between",
+              alignItems: "start",
+            }}
+          >
+            <h1>
+              Tables <a href="https://covidactnow.org">[source]</a>
+            </h1>
+            <FormGroup label="State">
+              <HTMLSelect
+                value={selectedState}
+                onChange={(e) => setSelectedState(e.target.value)}
+              >
+                {Object.entries(statesAbbr).map((state, index) => {
+                  return (
+                    <option value={state[1]} key={index}>
+                      {state[0]}
+                    </option>
+                  );
+                })}
+              </HTMLSelect>
+            </FormGroup>
+          </div>
+          <div style={{ padding: "1rem" }}>
+            <Dashboard stateCode={selectedState} />
+          </div>
+        </div>
+
+        <div
+          style={{
+            padding: "1rem",
+            backgroundColor: "white",
+            margin: "2rem",
+            borderRadius: "1rem",
+          }}
+        >
+          <div
+            style={{
+              display: "flex",
+              justifyContent: "space-between",
+              alignItems: "start",
+            }}
+          >
+            <h1>
+              Charts <a href="https://covidactnow.org">[source]</a>
+            </h1>
+            <FormGroup label="State">
+              <HTMLSelect
+                value={selectedState}
+                onChange={(e) => setSelectedState(e.target.value)}
+              >
+                {Object.entries(statesAbbr).map((state, index) => {
+                  return (
+                    <option value={state[1]} key={index}>
+                      {state[0]}
+                    </option>
+                  );
+                })}
+              </HTMLSelect>
+            </FormGroup>
+          </div>
+          <Graphs stateCode={selectedState} />
+        </div>
+      </main>
+
+      <footer>
+        <div
+          style={{
+            display: "flex",
+            justifyContent: "center",
+            gap: "0.5rem",
+            padding: "1rem",
+            width: "100%",
+            backgroundColor: "lightgray",
+          }}
+        >
+          <a
+            href="https://github.com/ethanl21/CovidMap"
+            rel="noopener"
+            target="_blank"
+          >
+            Source Code
+          </a>
+          •
+          <a href="https://disease.sh" rel="noopener" target="_blank">
+            Map Data Source
+          </a>
+          •
+          <a href="https://covidactnow.org" rel="noopener" target="_blank">
+            Tables and Charts Data Source
+          </a>
+        </div>
+      </footer>
     </>
   );
 }
